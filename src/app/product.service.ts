0391e3807e511efd2867ef13ec746af2f1994266
John Yaeger <49602916+john-yaeger@users.noreply.github.com>
2020-04-21 17:22:46 -0500

    Completed module 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-// import { map } from 'rxjs/add/operator/map';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+//import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -12,6 +12,7 @@ export class ProductService {
 
   getAlbum(id: number) {
 
-    return this._http.get(this._albumUrl).pipe(map(response => response.json()));
+    return this._http.get(this._albumUrl).map(response => response.json());
+    // return this._http.get(this._albumUrl).pipe(map(response => response.json()));
   }
 }
