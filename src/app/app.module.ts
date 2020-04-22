86678b5b7c9cdbf30bc6ce12a78d6436c512375c
John Yaeger <49602916+john-yaeger@users.noreply.github.com>
2020-04-21 20:27:03 -0500

    Completed module 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,19 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import {ProductService } from './product.service';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [{
+  path: 'products',
+  component: ProductListComponent
+}, {
+  path: 'product/:id',
+  component: ProductPageComponent
+}, {
+  path: '',
+  redirectTo: 'products',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   declarations: [
@@ -19,7 +32,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ProductService
